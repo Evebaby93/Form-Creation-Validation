4dc4df263cef5cdf2d5ef9e4528a6a75c21650a4
Evelyn Alamba <eala56847@gmail.com>
2025-06-23 16:20:38 +0100

    Create fetch-data.js

---
+++ fetch-data.js
@@ -0,0 +1,32 @@
+async function fetchUserData() {
+try {
+
+   const apiUrl = 'https://jsonplaceholder.typicode.com/users'
+  const dataContainer = document.getElementById("api-data")
+
+  const response = await fetch(apiUrl)
+
+  const users = await response.json()
+
+  dataContainer.innerHTML = ''
+
+  const userList = document.createElement("ul")
+
+  users.forEach(user => {
+const li = document.createElement("li")
+    li.textContent = user
+    userList.append(li)
+  })
+
+  dataContainer.append(userList)
+  
+}
+
+  catch(e) {
+    dataContainer.innerHTML = ''
+    dataContainer.textContent = "Failed to load user data."
+  }
+ 
+}
+
+document.addEventListener("DOMContentLoaded", fetchUserData)
